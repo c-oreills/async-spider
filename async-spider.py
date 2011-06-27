703f1553a5180fb7226fe49e956870e98d8c9f1b
Christy O'Reilly <christyor73@gmail.com>
2011-06-27 07:57:43 +0100

    Initial cut at asynchronous spider

---
+++ async-spider.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+"""
+Asyncronous spider to fetch and process web pages, looking for profanities to
+make mailing list moderation easier.
+"""
+
+from BeautifulSoup import BeautifulSoup
+from urlparse import urldefrag, urljoin, urlparse
+
+from gevent.pool import Pool
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
+
+import urllib2
+
+# Optional colourful printing
+try:
+    from termcolor import cprint
+except ImportError:
+    def cprint(string, *args):
+        print string
+
+PROFANITIES = [
+    'bunghole',
+    'crap',
+    'bowels',
+    'hosed',
+    'penistone',
+    'poop',
+    'sofa-king',
+    'testes',
+    'tool',
+    'turd',
+    ]
+
+try:
+    import django
+except ImportError:
+    pass
+else:
+    # Django shipped with a profanities list until 1.3
+    if django.VERSION < (1, 3):
+        from django.conf.global_settings import PROFANITIES_LIST
+        PROFANITIES.extend(PROFANITIES_LIST)
+
+START_URL = 'http://www.erfworld.com/wiki/index.php'
+START_URL = 'http://docs.python.org/library/index.html'
+MAX_DEPTH = 1
+
+
+def job(url, depth_left, pool):
+    """
+    Fetch a url, process it and add any urls in it to the job queue.
+    """
+    global seen_urls
+
+    data = fetch_url(url)
+    if data is None:
+        return
+
+    page_soup = BeautifulSoup(data)
+    process_page_soup(page_soup, url)
+
+    if depth_left >= 0:
+        rel_urls = get_page_links(page_soup)
+
+        for rel_url in rel_urls:
+            abs_url = parse_rel_url(rel_url, url)
+            if abs_url not in seen_urls:
+                seen_urls.add(abs_url)
+                pool.spawn(job, abs_url, depth_left-1, pool)
+
+
+def parse_rel_url(rel_url, url):
+    """
+    Turn a relative url into a normalised absolute url, without a fragment
+    """
+    abs_url = urljoin(url, rel_url)
+    new_url, frag = urldefrag(abs_url)
+    #new_url = parsed_url.geturl()
+    return new_url
+
+def fetch_url(url):
+    """
+    Attempt to fetch a url and return the data, with verbose output.
+    """
+    cprint('Starting %s' % url, 'green')
+    try:
+        data = urllib2.urlopen(url).read()
+    except urllib2.HTTPError as e:
+        cprint('Error for %s: %s' % (url, e,), 'red')
+        return None
+    else:
+        cprint('  %s: %s bytes: %r' % (url, len(data), data[:50]), 'cyan')
+        return data
+
+def process_page_soup(page_soup, url):
+    """
+    Process the web page as needed.
+    In this example, the page is scanned for profanities.
+    """
+    # Strip out all tags, leaving only text
+    page_text = ''.join(page_soup.findAll(text=True))
+
+    bad_words = [word for word in PROFANITIES if word in page_text.lower()]
+    if bad_words:
+        cprint('%s found in %s' % (', '.join(bad_words), url), 'white', 'on_red')
+        # Extension: Add this to results list to email to moderator
+
+def get_page_links(page_soup):
+    """
+    Return all urls on a page if they haven't already been seen.
+    """
+    rel_urls = []
+    for link in page_soup.findAll('a'):
+        attrs = dict(link.attrs)
+        if u'href' in attrs:
+            href = attrs[u'href'] 
+            rel_urls.append(href)
+    return rel_urls
+
+  
+# TODO: Currently blocks because of Greenlets not dying -
+# implement proper queueing
+seen_urls = set((START_URL,))
+pool = Pool(10)
+pool.spawn(job, START_URL, MAX_DEPTH, pool)
+#pool.join()
